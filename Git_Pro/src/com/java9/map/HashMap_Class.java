a6768af6b8226084f460117f5fea3c6fa1288522
arockiarajak <37239861+arockiarajak@users.noreply.github.com>
2018-05-09 12:33:17 +0530

    Update HashMap_Class.java

--- Git_Pro/src/com/java9/map/HashMap_Class.java
+++ Git_Pro/src/com/java9/map/HashMap_Class.java
@@ -11,5 +11,6 @@ public class HashMap_Class {
 		Map<String, Integer> hm = Map.of("hai", 1, "raj", 2, "ram", 3, "gopal", 4, "varma", 5, "saliya", 6, "behind", 7, "krackers", 8, "baseball", 9, "samy", 10);
 		
 		System.out.println(hm);
+		System.out.println("Newly added Statement");
 	}
 }

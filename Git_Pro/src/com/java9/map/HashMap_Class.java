73d8ea0406a6c08dd549b68e4d6b5afb1af5a644
arockiarajak <arockiarajak@gmail.com>
2018-05-09 12:17:34 +0530

    First Git Repository

---
+++ Git_Pro/src/com/java9/map/HashMap_Class.java
@@ -0,0 +1,15 @@
+package com.java9.map;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMap_Class {
+
+	public static void main(String[] args) {
+		
+		Map<String, Integer> hm = Map.of("hai", 1, "raj", 2, "ram", 3, "gopal", 4, "varma", 5, "saliya", 6, "behind", 7, "krackers", 8, "baseball", 9, "samy", 10);
+		
+		System.out.println(hm);
+	}
+}

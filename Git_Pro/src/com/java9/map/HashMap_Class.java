2f5ebf66a2e8a90f5554d5ec706bbfcf4cd9f663
arockiarajak1 <arockiarajak@gmail.com>
2018-05-09 12:44:59 +0530

    Simple Changes

--- Git_Pro/src/com/java9/map/HashMap_Class.java
+++ Git_Pro/src/com/java9/map/HashMap_Class.java
@@ -8,8 +8,8 @@ public class HashMap_Class {
 
 	public static void main(String[] args) {
 		
-		Map<String, Integer> hm = Map.of("hai", 1, "raj", 2, "ram", 3, "gopal", 4, "varma", 5, "saliya", 6, "behind", 7, "krackers", 8, "baseball", 9, "samy", 10);
-		
+		Map<String, Integer> hm = new HashMap<String, Integer>();
+		hm.put("raj", 1);
 		System.out.println(hm);
 		System.out.println("Newly added Statement");
 	}
